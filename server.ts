bb6f995133e36e2c6dc7279a001db3f7546dd9a3
Mahmoud Hmmadia <102804429+MahmoudHmmadia@users.noreply.github.com>
2024-07-18 15:35:39 +0300

    fix

--- server.ts
+++ server.ts
@@ -20,7 +20,7 @@ mainConfig(server);
 
 router(server);
 
-mongoose.connect(process.env.DB_URL!, { dbName: "akm" }).then(() => {
+mongoose.connect(process.env.DB_URL!, { dbName: "AKM" }).then(() => {
   server.listen(process.env.PORT, async () => {
     console.log("Server Up And Running On Port: " + process.env.PORT);
   });
